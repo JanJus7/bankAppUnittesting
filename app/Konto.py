cad83427dd0f1a5bc6772f15f7bd766ac9cf1845
Jan <janjus2004@gmail.com>
2024-10-16 00:44:54 +0200

    zrobione do feature 5

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,7 @@
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, imie, nazwisko, numerPESEL, promoCode):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
+        self.numerPESEL = numerPESEL
+        self.promoCode = promoCode
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -7,9 +7,32 @@ class TestCreateBankAccount(unittest.TestCase):
     def test_tworzenie_konta(self):
         imie = "Dariusz"
         nazwisko = "Januszewski"
-        pierwsze_konto = Konto(imie, nazwisko)
+        numerPESEL = "04251010644"
+        promoCode = "PROM_IOI"
+        pierwsze_konto = Konto(imie, nazwisko, numerPESEL, promoCode)
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.numerPESEL, "04251010644", "Pesel nie został zapisany!")
+        self.assertRegex(pierwsze_konto.numerPESEL, r"^[0-9]{11}$", "Niepoprawny pesel!")
+        
+        rok_urodzenia = int(pierwsze_konto.numerPESEL[:2])
+        miesiac_urodzenia = int(pierwsze_konto.numerPESEL[2:4])
+
+        if miesiac_urodzenia >= 20:
+            rok_urodzenia += 2000
+            miesiac_urodzenia -= 20
+        else:
+            rok_urodzenia += 1900
+        
+        if promoCode != None:
+            if rok_urodzenia >= 1960:
+                self.assertRegex(pierwsze_konto.promoCode, r"^PROM\_[A-Z]{3}", "Niepoprawny kod promocyjny!")
+                pierwsze_konto.saldo += 50
+                print(pierwsze_konto.saldo)
+            else:
+                print("jesteś za stary!")
+
+
 
     #tutaj proszę dodawać nowe testy

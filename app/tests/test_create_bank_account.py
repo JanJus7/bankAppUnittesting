1eb39053e6f5004c1bc34048b9294e248943c63e
Jan <janjus2004@gmail.com>
2024-10-22 11:19:08 +0200

    Lekko zmieniony wizualnie kod, dodane defy itp.

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -4,20 +4,23 @@ from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
 
+    def setUp(self):
+        self.imie = "Dariusz"
+        self.nazwisko = "Januszewski"
+        self.numerPESEL = "04251010644"
+        self.promoCode = "PROM_IOI"
+        self.pierwsze_konto = Konto(self.imie, self.nazwisko, self.numerPESEL, self.promoCode)
+
     def test_tworzenie_konta(self):
-        imie = "Dariusz"
-        nazwisko = "Januszewski"
-        numerPESEL = "04251010644"
-        promoCode = "PROM_IOI"
-        pierwsze_konto = Konto(imie, nazwisko, numerPESEL, promoCode)
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-        self.assertEqual(pierwsze_konto.numerPESEL, "04251010644", "Pesel nie został zapisany!")
-        self.assertRegex(pierwsze_konto.numerPESEL, r"^[0-9]{11}$", "Niepoprawny pesel!")
+        self.assertEqual(self.pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
+        self.assertEqual(self.pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
+        self.assertEqual(self.pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(self.pierwsze_konto.numerPESEL, "04251010644", "Pesel nie został zapisany!")
+        self.assertRegex(self.pierwsze_konto.numerPESEL, r"^[0-9]{11}$", "Niepoprawny pesel!")
         
-        rok_urodzenia = int(pierwsze_konto.numerPESEL[:2])
-        miesiac_urodzenia = int(pierwsze_konto.numerPESEL[2:4])
+    def test_promo(self):    
+        rok_urodzenia = int(self.pierwsze_konto.numerPESEL[:2])
+        miesiac_urodzenia = int(self.pierwsze_konto.numerPESEL[2:4])
 
         if miesiac_urodzenia >= 20:
             rok_urodzenia += 2000
@@ -25,14 +28,13 @@ class TestCreateBankAccount(unittest.TestCase):
         else:
             rok_urodzenia += 1900
         
-        if promoCode != None:
+        if self.promoCode != None:
             if rok_urodzenia >= 1960:
-                self.assertRegex(pierwsze_konto.promoCode, r"^PROM\_[A-Z]{3}", "Niepoprawny kod promocyjny!")
-                pierwsze_konto.saldo += 50
-                print(pierwsze_konto.saldo)
+                self.assertRegex(self.pierwsze_konto.promoCode, r"^PROM\_[A-Z]{3}", "Niepoprawny kod promocyjny!")
+                self.pierwsze_konto.saldo += 50
+                print(self.pierwsze_konto.saldo)
             else:
                 print("jesteś za stary!")
-
-
+    
 
     #tutaj proszę dodawać nowe testy

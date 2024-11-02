0f73a5c5fb67fc985826681c784e176713405736
Jan <janjus2004@gmail.com>
2024-11-02 22:22:56 +0100

    lab no.2 caught up! :)

--- app/Konto.py
+++ app/Konto.py
@@ -9,7 +9,18 @@ class Konto:
         self.promoCode = promoCode
         #older method. RE should be better :)
         # if promoCode and promoCode.startswith("PROM_"):
-        if promoCode and re.match("PROM_[A-Z]{3}", promoCode):
+        if promoCode and re.match("PROM_[A-Z]{3}", promoCode) and self.isYoungEnough():
             self.saldo += 50
     # def incomingTransfer(self, kwota):
     #     self.saldo += kwota
+
+    def isYoungEnough(self):
+        year = int(self.numerPESEL[:2])
+        month = int(self.numerPESEL[2:4])
+
+        if 1 <= month <= 12:
+            year += 1900  
+        elif 21 <= month <= 32:
+            year += 2000  
+
+        return year > 1960
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -7,10 +7,10 @@ class CreateBankAccount(unittest.TestCase):
     def setUp(self):
         self.imie = "Dariusz"
         self.nazwisko = "Januszewski"
-        self.numerPESEL = "04251010644"
 
     def testTworzenieKonta(self):
-        pierwsze_konto = Konto(self.imie, self.nazwisko, self.numerPESEL)
+        numerPESEL = "04251010644"
+        pierwsze_konto = Konto(self.imie, self.nazwisko, numerPESEL)
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
@@ -18,10 +18,17 @@ class CreateBankAccount(unittest.TestCase):
         self.assertRegex(pierwsze_konto.numerPESEL, r"^[0-9]{11}$", "Niepoprawny pesel!")
         
     def testPromoTrue(self):  
-        promoCode = "PROM_ABC"     
-        pierwsze_konto = Konto(self.imie, self.nazwisko, self.numerPESEL, promoCode)
+        promoCode = "PROM_ABC"   
+        numerPESEL = "04251010644"  
+        pierwsze_konto = Konto(self.imie, self.nazwisko, numerPESEL, promoCode)
         self.assertEqual(pierwsze_konto.saldo, 50, "Saldo nie jest 50!")
         self.assertRegex(pierwsze_konto.promoCode, promoCode, "Niepoprawny kod promocyjny!")
+
+    def testPromoDeclined(self):
+        promoCode = "PROM_ABC" 
+        numerPESEL = "04051010644" 
+        pierwsze_konto = Konto(self.imie, self.nazwisko, numerPESEL, promoCode)
+        self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest 0")
 
 # class TestTransfers(CreateBankAccount):
 #     def testIncomes(self):

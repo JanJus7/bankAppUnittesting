8ec4754bf6b537f033047bb369aa7e01ec65721c
Jan <janjus2004@gmail.com>
2025-01-26 23:39:06 +0100

    tried to implement the second perf test

--- app/performanceTests/api_performance_test.py
+++ app/performanceTests/api_performance_test.py
@@ -3,7 +3,6 @@ import requests
 import time
 import random
 
-# API address
 API_URL = "http://127.0.0.1:5000"
 
 class TestApiPerformance(unittest.TestCase):
@@ -42,3 +41,36 @@ class TestApiPerformance(unittest.TestCase):
                 200,
                 f"Account deletion error {i+1}: {response.status_code}"
             )
+
+    def create_account_and_send_transfers(self):
+        pesel = "12345678901"
+        requests.post(
+                f"{API_URL}/api/accounts",
+                json={"name": "Jan", "surname": "Kowalski", "pesel": pesel},
+                timeout=0.5
+            )
+        for i in range(100):
+            start_time = time.time()
+            response = requests.post(
+                f"{API_URL}/api/accounts/{pesel}/transfer",
+                json={"type": "incoming", "amount": 10},
+                timeout=0.5
+            )
+            end_time = time.time()
+        
+            self.assertLess(
+                end_time - start_time,
+                0.5,
+                f"Recieving transfer nr.{i+1} exceeded the time limit."
+            )
+            self.assertEqual(
+                response.status_code,
+                200,
+                f"Transfer error {i+1}: {response.status_code}"
+            )
+        response = requests.get(f"{API_URL}/api/accounts/{pesel}")
+        data = response.json()
+        self.assertEqual(data["balance"], 1000)
+
+        
+    

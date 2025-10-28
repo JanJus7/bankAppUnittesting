1b7cea4d634d164a919d221bef52e501d8bbe54f
Jan <janjus2004@gmail.com>
2025-10-28 19:27:59 +0100

    test

--- app/tests/test_create_company_account.py
+++ app/tests/test_create_company_account.py
@@ -16,7 +16,7 @@ class TestCreateCompanyAccount(unittest.TestCase):
     
     @parameterized.expand([
         ("0123456789", "0123456789"),
-        ("aaaaaaaaaa", "Niepoprawny NIP!"),
+        ("aaaaaaaaab", "Niepoprawny NIP!"),
         ("123456789o", "Niepoprawny NIP!"),
         ("aaa1234567890aaa", "Niepoprawny NIP!"),
         ("1234567891234", "Niepoprawny NIP!"),
